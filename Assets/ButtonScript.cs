c204be12189d3891f457c2e578fa648cc25d497e
JohnPhilipPearson <pdjpearson@me.com>
2017-04-05 10:39:34 -0400

    first time add

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	// Navigate to main scene
+	public void StartScene()
+	{
+		// We will load the first scene (this is scene 0)
+		UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+	}
+}
